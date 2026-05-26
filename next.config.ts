aa71dadc6f5e4ba8d8e2e9d2bef9b8dd7793a539
Astha <astha@Asthas-MacBook-Air.local>
2026-05-26 17:14:24 +0530

    Simplify codebase, enable static export, and configure GitHub Pages deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
-  images: {
-    qualities: [75, 90],
-  },
 };
 
 export default nextConfig;
